de89f1354080b1bc6708fe429dfefe8c557dbc55
Daniel Ecer <de-code@users.noreply.github.com>
2017-10-24 14:48:52 +0100

    adder setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import os
+
+from setuptools import (
+  find_packages,
+  setup
+)
+
+with open(os.path.join('requirements.txt'), 'r') as f:
+  REQUIRED_PACKAGES = f.readlines()
+
+packages = find_packages()
+
+setup(
+  name='sciencebeam_judge',
+  version='0.0.1',
+  install_requires=REQUIRED_PACKAGES,
+  packages=packages,
+  include_package_data=True,
+  description='ScienceBeam Judge'
+)
