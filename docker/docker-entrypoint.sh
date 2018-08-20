f6ab8023d31d79ec9675bab6ecf038d731ffbe1b
Daniel Ecer <de-code@users.noreply.github.com>
2018-08-20 12:39:14 +0100

    added umask

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-# woraround for permissions issue (to allow notebooks to be updated and added from the container)
+# workaround for permissions issue (to allow notebooks to be updated and added from the container)
 
+umask +rw
 chmod -R a+w .
 
 ls -la
