6c85bff4410b276217b2185fd7a4b028be2fbf1d
Daniel Ecer <de-code@users.noreply.github.com>
2018-08-20 15:15:47 +0100

    added script to update example data results

---
+++ update-example-data-results.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+docker-compose build sciencebeam-judge
+
+cwd="$(pwd)"
+
+update_results() {
+    tool=$1
+    docker-compose run \
+        --volume="$cwd/example-data:/example-data" \
+        sciencebeam-judge \
+        ./evaluate.sh \
+        --target-file-list /example-data/pmc-sample-1943-cc-by-subset/file-list.tsv \
+        --target-file-column=xml_url \
+        --prediction-file-list /example-data/pmc-sample-1943-cc-by-subset-results/file-list-$tool.lst \
+        --output-path /example-data/pmc-sample-1943-cc-by-subset-results/evaluation-results/$tool \
+        --sequential
+}
+
+update_results cermine
+update_results grobid-tei
+
+docker-compose down -v
