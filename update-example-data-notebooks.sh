36bc01d6a1e932917f5708369e4a41bc7619548b
Daniel Ecer <de-code@users.noreply.github.com>
2019-01-11 10:16:40 +0000

    added script to update notebooks and check for errors

---
+++ update-example-data-notebooks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+docker-compose build sciencebeam-judge-jupyter
+
+cwd="$(pwd)"
+
+update_notebook() {
+    notebook_file=$1
+    docker-compose run --rm \
+        sciencebeam-judge-jupyter \
+        jupyter nbconvert --to notebook --execute --inplace ./$notebook_file
+    stderr_content=$(
+        docker-compose run --rm --entrypoint 'sh -c' sciencebeam-judge-jupyter \
+        "grep 'stderr' ./$notebook_file" | cat
+    )
+    if [ ! -z "$stderr_content" ]; then
+        echo "Error: Notebook contains stderr output"
+        exit 3
+    fi
+}
+
+update_notebook conversion-results-summary.ipynb
+update_notebook conversion-results-details.ipynb
+
+docker-compose down -v
