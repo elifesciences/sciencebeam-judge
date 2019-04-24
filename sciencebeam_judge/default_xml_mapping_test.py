d0574a78a02fd96abbc99037bc5f1112514461d5
Daniel Ecer <de-code@users.noreply.github.com>
2019-04-24 13:45:56 +0100

    added tei training data mapping

--- sciencebeam_judge/default_xml_mapping_test.py
+++ sciencebeam_judge/default_xml_mapping_test.py
@@ -23,38 +23,60 @@ def _default_xml_mapping():
     return parse_xml_mapping('./xml-mapping.conf')
 
 
+def _parse_xml(*args, **kwargs):
+    result = parse_xml(*args, **kwargs)
+    LOGGER.debug('result: %s', result)
+    return result
+
+
 class TestDefaultXmlMapping(object):
-    class TestTeiAbstractText(object):
-        def test_should_return_without_paragraph(self, default_xml_mapping):
-            xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(
-                'abstract1'
-            ))))
-            result = parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
-            LOGGER.debug('result: %s', result)
-            assert result.get('abstract') == ['abstract1']
+    class TestTei(object):
+        class TestTeiAbstractText(object):
+            def test_should_return_without_paragraph(self, default_xml_mapping):
+                xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(
+                    'abstract1'
+                ))))
+                result = _parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
+                assert result.get('abstract') == ['abstract1']
 
-        def test_should_return_with_div_and_paragraph(self, default_xml_mapping):
-            xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(E.div(
-                E.p('abstract1')
-            )))))
-            result = parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
-            LOGGER.debug('result: %s', result)
-            assert result.get('abstract') == ['abstract1']
+            def test_should_return_with_div_and_paragraph(self, default_xml_mapping):
+                xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(E.div(
+                    E.p('abstract1')
+                )))))
+                result = _parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
+                assert result.get('abstract') == ['abstract1']
 
-        def test_should_ignore_first_head(self, default_xml_mapping):
-            xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(E.div(
-                E.head('Abstract'),
-                E.p('abstract1')
-            )))))
-            result = parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
-            LOGGER.debug('result: %s', result)
-            assert result.get('abstract') == ['abstract1']
+            def test_should_ignore_first_head(self, default_xml_mapping):
+                xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(E.div(
+                    E.head('Abstract'),
+                    E.p('abstract1')
+                )))))
+                result = _parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
+                assert result.get('abstract') == ['abstract1']
 
-        def test_not_should_ignore_further_head_elements(self, default_xml_mapping):
-            xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(
-                E.div(E.head('Abstract')),
-                E.div(E.head('Sub:'), E.p('abstract1'))
-            ))))
-            result = parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
-            LOGGER.debug('result: %s', result)
-            assert result.get('abstract') == ['Sub: abstract1']
+            def test_not_should_ignore_further_head_elements(self, default_xml_mapping):
+                xml = E.TEI(E.teiHeader(E.profileDesc(E.abstract(
+                    E.div(E.head('Abstract')),
+                    E.div(E.head('Sub:'), E.p('abstract1'))
+                ))))
+                result = _parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
+                assert result.get('abstract') == ['Sub: abstract1']
+
+    class TestTeiTraining(object):
+        class TestTeiTrainingTitle(object):
+            def test_should_extract_title(self, default_xml_mapping):
+                xml = E.tei(E.text(E.front(
+                    E.docTitle(E.titlePart('Title 1')),
+                    E.note('other')
+                )))
+                result = _parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
+                assert result.get('title') == ['Title 1']
+
+        class TestTeiTrainingAbstract(object):
+            def test_should_extract_title(self, default_xml_mapping):
+                xml = E.tei(E.text(E.front(
+                    E.div('Abstract 1', type='abstract'),
+                    E.div('other')
+                )))
+                result = _parse_xml(BytesIO(etree.tostring(xml)), xml_mapping=default_xml_mapping)
+                assert result.get('abstract') == ['Abstract 1']
