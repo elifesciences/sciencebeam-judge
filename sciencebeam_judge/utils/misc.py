7871437b11a7cc8c81358233d406c96e3fcd725b
Daniel Ecer <de-code@users.noreply.github.com>
2020-08-04 19:49:42 +0200

    normalize reference author name (#258)
    * normalize reference author names
    
    * use regex for upper case character class
    
    * added trailing line feed

---
+++ sciencebeam_judge/utils/misc.py
@@ -0,0 +1,25 @@
+import regex
+
+
+# Note: `\p{Lu}` is refering to the upper case character class
+
+def remove_dot_after_initials(person_name: str) -> str:
+    return regex.sub(
+        r'([\p{Lu}])\.\s?',
+        r'\1 ',
+        person_name
+    )
+
+
+def remove_space_between_initials(person_name: str) -> str:
+    return regex.sub(
+        r'(?<=[\p{Lu}])(\s)(?=[\p{Lu}]\b)',
+        r'',
+        person_name
+    )
+
+
+def normalize_person_name(person_name: str) -> str:
+    return remove_space_between_initials(
+        remove_dot_after_initials(person_name)
+    )
