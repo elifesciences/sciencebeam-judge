2afcd295c41dec89e3e82013e02dee1cb1499308
Daniel Ecer <de-code@users.noreply.github.com>
2021-02-05 14:51:47 +0000

    refactored and extracted distance_matching

---
+++ sciencebeam_judge/utils/distance_matching.py
@@ -0,0 +1,177 @@
+import logging
+
+from collections import Counter
+from typing import Callable, Iterable, List, NamedTuple, Set
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+T_Distance_Function = Callable[[str, str], float]
+
+
+DEFAULT_THRESHOLD = 0.5
+
+
+class DistanceMeasure(T_Distance_Function):
+    def __init__(
+        self,
+        distance_fn: T_Distance_Function,
+        approximate_distance_fn_list: List[T_Distance_Function] = None
+    ):
+        self._distance_fn = distance_fn
+        self._approximate_distance_fn_list = approximate_distance_fn_list or []
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self._distance_fn}, {self._approximate_distance_fn_list})'
+
+    def __call__(self, value_1: str, value_2: str) -> float:
+        return self._distance_fn(value_1, value_2)
+
+    @property
+    def approximations(self) -> List[T_Distance_Function]:
+        return self._approximate_distance_fn_list
+
+
+class DistanceMatchResult(NamedTuple):
+    value_1: str
+    value_2: str
+    score: float
+
+
+class DistanceMatch(DistanceMatchResult):
+    pass
+
+
+class DistanceMismatch(DistanceMatchResult):
+    pass
+
+
+def get_score_for_match_count(match_count: int, length: int):
+    if length:
+        return 2.0 * match_count / length
+    return 1.0
+
+
+def get_length_based_upper_bound_score(value_1: str, value_2: str) -> float:
+    # See difflib:SequenceMatcher.real_quick_ratio
+    length_1 = len(value_1)
+    length_2 = len(value_2)
+    max_length = max(length_1, length_2)
+    if not max_length:
+        return 1.0
+    # can't have more matches than the number of elements in the
+    # shorter sequence
+    return min(length_1, length_2) / max_length
+
+
+def get_character_count_based_upper_bound_score(value_1: str, value_2: str) -> float:
+    # See difflib:SequenceMatcher.quick_ratio
+    max_length = max(len(value_1), len(value_2))
+    if not max_length:
+        return 1.0
+    # viewing a and b as multisets, set matches to the cardinality
+    # of their intersection; this counts the number of matches
+    # without regard to order, so is clearly an upper bound
+    value_counts_1 = Counter(value_1)
+    value_counts_2 = Counter(value_2)
+    matches = sum(
+        min(count_1, value_counts_2[c])
+        for c, count_1 in value_counts_1.items()
+    )
+    return matches / max_length
+
+
+def find_best_match(
+    value: str,
+    other_values: List[str],
+    distance_measure: DistanceMeasure,
+    threshold: float = DEFAULT_THRESHOLD,
+    approximate_threshold: float = None
+) -> DistanceMatchResult:
+
+    if approximate_threshold is None:
+        approximate_threshold = threshold
+    best_value = None
+    best_score = -1.0
+    remaining_other_values = other_values
+    for approximate_distance_fn in distance_measure.approximations:
+        remaining_other_values_with_score = sorted([
+            (approximate_distance_fn(value, other_value), other_value)
+            for other_value in remaining_other_values
+        ], reverse=True)
+        remaining_other_values = [
+            other_value for _, other_value in remaining_other_values_with_score
+        ]
+        if len(remaining_other_values_with_score) <= 1:
+            break
+    for other_value in remaining_other_values:
+        score = distance_measure(value, other_value)
+        LOGGER.debug(
+            'find_best_match, value=%r, other_value=%r, score=%s',
+            value, other_value, score
+        )
+        if score > best_score:
+            best_value = other_value
+            best_score = score
+            if best_score == 1.0:
+                break
+
+    LOGGER.debug(
+        'find_best_match, value=%r, best_value=%r, best_score=%s',
+        value, best_value, best_score
+    )
+    if best_score >= 0.0 and best_score >= threshold:
+        return DistanceMatch(value_1=value, value_2=best_value, score=best_score)
+    return None
+
+
+def iter_distance_matches(
+    set_1: Set[str],
+    set_2: Set[str],
+    distance_measure: DistanceMeasure,
+    threshold: float = DEFAULT_THRESHOLD,
+    mismatch_threshold: float = 0.0
+) -> Iterable[DistanceMatchResult]:
+    unmatched_set_1 = []
+    remaining_set_2 = list(set_2)
+    for value_1 in set_1:
+        best_match = find_best_match(
+            value_1,
+            remaining_set_2,
+            distance_measure=distance_measure,
+            threshold=threshold
+        )
+        if best_match:
+            yield best_match
+            remaining_set_2.remove(best_match.value_2)
+        else:
+            unmatched_set_1.append(value_1)
+
+    for value_1 in unmatched_set_1[:len(remaining_set_2)]:
+        best_match = find_best_match(
+            value_1,
+            remaining_set_2,
+            distance_measure=distance_measure,
+            threshold=mismatch_threshold
+        )
+        LOGGER.debug(
+            'unmatched item, best match (%s), pair with expected,'
+            ' expected_item=%s, matching_item=%s',
+            distance_measure, value_1, best_match
+        )
+        if best_match:
+            yield DistanceMismatch(
+                value_1=value_1, value_2=best_match.value_2, score=best_match.score
+            )
+            unmatched_set_1.remove(value_1)
+            remaining_set_2.remove(best_match.value_2)
+
+    for value_1 in unmatched_set_1:
+        yield DistanceMismatch(value_1=value_1, value_2=None, score=0.0)
+    for value_2 in remaining_set_2:
+        yield DistanceMismatch(value_1=None, value_2=value_2, score=0.0)
+
+
+def get_distance_matches(*args, **kwargs) -> List[DistanceMatchResult]:
+    return list(iter_distance_matches(*args, **kwargs))
---
+++ tests/utils/distance_matching_test.py
@@ -0,0 +1,159 @@
+import pytest
+
+from sciencebeam_judge.evaluation.scoring_methods import levenshtein_score
+
+from sciencebeam_judge.utils.distance_matching import (
+    DistanceMeasure,
+    DistanceMatch,
+    DistanceMismatch,
+    get_length_based_upper_bound_score,
+    get_character_count_based_upper_bound_score,
+    get_distance_matches
+)
+
+
+DEFAULT_DISTANCE_MEASURE = DistanceMeasure(levenshtein_score)
+
+DISTANCE_MEASURE_WITH_APPROXIMATE = DistanceMeasure(
+    levenshtein_score,
+    [
+        get_length_based_upper_bound_score,
+        get_character_count_based_upper_bound_score
+    ]
+)
+
+
+@pytest.fixture(name='distance_measure', params=[
+    DEFAULT_DISTANCE_MEASURE, DISTANCE_MEASURE_WITH_APPROXIMATE
+])
+def distance_measure_fixture(request):
+    return request.param
+
+
+class TestGetLengthBasedUpperBoundScore:
+    def test_should_return_one_for_two_empty_strings(self):
+        assert get_length_based_upper_bound_score('', '') == 1.0
+
+    def test_should_return_zero_if_only_one_value_is_empty(self):
+        assert get_length_based_upper_bound_score('abc', '') == 0.0
+        assert get_length_based_upper_bound_score('', 'abc') == 0.0
+
+    def test_should_return_dot_five_if_one_value_is_twice_as_long(self):
+        assert get_length_based_upper_bound_score('abc', '123456') == 0.5
+        assert get_length_based_upper_bound_score('123456', 'abc') == 0.5
+
+    def test_should_return_dot_two_five_if_one_value_is_four_times_as_long(self):
+        assert get_length_based_upper_bound_score('ab', '12345678') == 0.25
+        assert get_length_based_upper_bound_score('12345678', 'ab') == 0.25
+
+
+class TestGetCharacterCountBasedUpperBoundScore:
+    def test_should_return_one_for_two_empty_strings(self):
+        assert get_character_count_based_upper_bound_score('', '') == 1.0
+
+    def test_should_return_zero_if_only_one_value_is_empty(self):
+        assert get_character_count_based_upper_bound_score('abc', '') == 0.0
+        assert get_character_count_based_upper_bound_score('', 'abc') == 0.0
+
+    def test_should_return_one_if_both_values_contain_same_characters(self):
+        assert get_character_count_based_upper_bound_score('abc', 'cba') == 1.0
+        assert get_character_count_based_upper_bound_score('cba', 'abc') == 1.0
+
+    def test_should_return_dot_five_if_values_contain_half_the_characters(self):
+        assert get_character_count_based_upper_bound_score('abcd', 'ba') == 0.5
+        assert get_character_count_based_upper_bound_score('ba', 'abcd') == 0.5
+
+    def test_should_return_dot_five_if_values_share_half_of_the_characters(self):
+        assert get_character_count_based_upper_bound_score('abcd', 'baxx') == 0.5
+        assert get_character_count_based_upper_bound_score('baxx', 'abcd') == 0.5
+
+    def test_should_return_dot_five_if_values_share_half_of_the_character_counts(self):
+        assert get_character_count_based_upper_bound_score('aaab', 'bbba') == 0.5
+        assert get_character_count_based_upper_bound_score('bbba', 'aaab') == 0.5
+
+
+class TestGetDistanceMatches:
+    def test_should_return_empty_list_for_empty_inputs(self, distance_measure: DistanceMeasure):
+        assert get_distance_matches([], [], distance_measure) == []
+
+    def test_should_return_single_item_for_single_exact_match(
+        self, distance_measure: DistanceMeasure
+    ):
+        assert get_distance_matches(
+            ['a'],
+            ['a'],
+            distance_measure
+        ) == [
+            DistanceMatch(value_1='a', value_2='a', score=1.0)
+        ]
+
+    def test_should_return_multiple_exact_matches(self, distance_measure: DistanceMeasure):
+        assert get_distance_matches(
+            ['a', 'b', 'c'],
+            ['c', 'b', 'a'],
+            distance_measure
+        ) == [
+            DistanceMatch(value_1='a', value_2='a', score=1.0),
+            DistanceMatch(value_1='b', value_2='b', score=1.0),
+            DistanceMatch(value_1='c', value_2='c', score=1.0)
+        ]
+
+    def test_should_return_single_item_with_similar_match(
+        self, distance_measure: DistanceMeasure
+    ):
+        assert get_distance_matches(
+            ['0123456789'],
+            ['012345678X'],
+            distance_measure
+        ) == [
+            DistanceMatch(
+                value_1='0123456789',
+                value_2='012345678X',
+                score=0.9
+            )
+        ]
+
+    def test_should_return_multiple_similar_matches(self, distance_measure: DistanceMeasure):
+        assert get_distance_matches(
+            ['0123456789', 'b', 'c'],
+            ['c', 'b', '012345678X'],
+            distance_measure
+        ) == [
+            DistanceMatch(value_1='0123456789', value_2='012345678X', score=0.9),
+            DistanceMatch(value_1='b', value_2='b', score=1.0),
+            DistanceMatch(value_1='c', value_2='c', score=1.0)
+        ]
+
+    def test_should_return_missing_items(self, distance_measure: DistanceMeasure):
+        assert get_distance_matches(
+            ['a', 'b', 'x'],
+            ['b', 'a'],
+            distance_measure
+        ) == [
+            DistanceMatch(value_1='a', value_2='a', score=1.0),
+            DistanceMatch(value_1='b', value_2='b', score=1.0),
+            DistanceMismatch(value_1='x', value_2=None, score=0.0)
+        ]
+
+    def test_should_return_extra_items(self, distance_measure: DistanceMeasure):
+        assert get_distance_matches(
+            ['a', 'b'],
+            ['x', 'b', 'a'],
+            distance_measure
+        ) == [
+            DistanceMatch(value_1='a', value_2='a', score=1.0),
+            DistanceMatch(value_1='b', value_2='b', score=1.0),
+            DistanceMismatch(value_1=None, value_2='x', score=0.0)
+        ]
+
+    def test_should_return_closest_mismatches(self, distance_measure: DistanceMeasure):
+        assert get_distance_matches(
+            ['a', 'b', '*xy*'],
+            ['+xy+', 'b', 'a'],
+            distance_measure,
+            threshold=0.8
+        ) == [
+            DistanceMatch(value_1='a', value_2='a', score=1.0),
+            DistanceMatch(value_1='b', value_2='b', score=1.0),
+            DistanceMismatch(value_1='*xy*', value_2='+xy+', score=0.5)
+        ]
