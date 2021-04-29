70a443d32a134035a0d4e08271c08b2b1c32d35e
Daniel Ecer <de-code@users.noreply.github.com>
2021-04-29 11:56:32 +0100

    added lower level fuzzy match test

--- tests/evaluation/special_evaluation/deleted_text_test.py
+++ tests/evaluation/special_evaluation/deleted_text_test.py
@@ -1,4 +1,29 @@
-from sciencebeam_judge.evaluation.special_evaluation.deleted_text import DeletedTextEvaluation
+from sciencebeam_judge.evaluation.special_evaluation.deleted_text import (
+    get_fuzzy_matched_text_fragments,
+    DeletedTextEvaluation
+)
+
+
+class TestGetFuzzyMatchedTextFragments:
+    def test_should_return_complete_match(self):
+        result = get_fuzzy_matched_text_fragments(
+            expected=['value1'],
+            actual=['value1']
+        )
+        assert len(result) == 1
+        assert result[0].value_1.text == 'value1'
+        assert result[0].value_2.text == 'value1'
+        assert result[0].score == 1.0
+
+    def test_should_return_mismatch(self):
+        result = get_fuzzy_matched_text_fragments(
+            expected=['abc'],
+            actual=['123']
+        )
+        assert len(result) >= 1
+        assert result[0].value_1.text == 'abc'
+        assert result[0].value_2 is None
+        assert result[0].score == 0.0
 
 
 class TestDeletedTextEvaluation:
