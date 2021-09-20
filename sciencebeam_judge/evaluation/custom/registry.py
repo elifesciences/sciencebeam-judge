59e873c2bbe365b53fb69d6d2dfc6e05131a6b40
Daniel Ecer <de-code@users.noreply.github.com>
2021-09-20 21:35:51 +0100

    added bounding_box_intersection to registry

--- sciencebeam_judge/evaluation/custom/registry.py
+++ sciencebeam_judge/evaluation/custom/registry.py
@@ -2,10 +2,14 @@ from sciencebeam_judge.evaluation.custom import CustomEvaluation
 from sciencebeam_judge.evaluation.custom.deleted_text import (
     DeletedTextEvaluation
 )
+from sciencebeam_judge.evaluation.custom.bounding_box_intersection import (
+    BoundingBoxIntersectionEvaluation
+)
 
 
 CUSTOM_EVALUATION_CLASS_BY_NAME = {
-    'deleted_text': DeletedTextEvaluation
+    'deleted_text': DeletedTextEvaluation,
+    'bounding_box_intersection': BoundingBoxIntersectionEvaluation
 }
 
 
