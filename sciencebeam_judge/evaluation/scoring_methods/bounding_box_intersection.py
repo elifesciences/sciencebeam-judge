502d002ba0f71ae9d502658a013c0bfde5b3d68c
Daniel Ecer <de-code@users.noreply.github.com>
2021-09-20 16:01:37 +0100

    calculate intersection

--- sciencebeam_judge/evaluation/scoring_methods/bounding_box_intersection.py
+++ sciencebeam_judge/evaluation/scoring_methods/bounding_box_intersection.py
@@ -1,6 +1,24 @@
 from typing import NamedTuple, Sequence
 
 
+class BoundingRange(NamedTuple):
+    start: float
+    length: float
+
+    def validate(self) -> 'BoundingRange':
+        if self.length < 0:
+            raise ValueError(f'length must not be less than zero, was: {self.length}')
+        return self
+
+    def intersection(self, other: 'BoundingRange') -> 'BoundingRange':
+        intersection_start = max(self.start, other.start)
+        intersection_end = min(self.start + self.length, other.start + other.length)
+        return BoundingRange(
+            intersection_start,
+            max(0, intersection_end - intersection_start)
+        )
+
+
 class BoundingBox(NamedTuple):
     x: float
     y: float
@@ -10,6 +28,24 @@ class BoundingBox(NamedTuple):
     @property
     def area(self) -> float:
         return self.width * self.height
+
+    @property
+    def x_range(self):
+        return BoundingRange(self.x, self.width).validate()
+
+    @property
+    def y_range(self):
+        return BoundingRange(self.y, self.height).validate()
+
+    def intersection(self, other: 'BoundingBox') -> 'BoundingBox':
+        intersection_x_range = self.x_range.intersection(other.x_range)
+        intersection_y_range = self.y_range.intersection(other.y_range)
+        return BoundingBox(
+            intersection_x_range.start,
+            intersection_y_range.start,
+            intersection_x_range.length,
+            intersection_y_range.length
+        )
 
 
 class PageBoundingBox(NamedTuple):
