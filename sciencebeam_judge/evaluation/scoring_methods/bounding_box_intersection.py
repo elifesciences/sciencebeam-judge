2bf5fbe623c0e053ea962043b2fff7e8c6686ff6
Daniel Ecer <de-code@users.noreply.github.com>
2021-09-20 16:06:17 +0100

    moved parse_page_bounding_box_list out of class

--- sciencebeam_judge/evaluation/scoring_methods/bounding_box_intersection.py
+++ sciencebeam_judge/evaluation/scoring_methods/bounding_box_intersection.py
@@ -52,34 +52,34 @@ class PageBoundingBox(NamedTuple):
     page_number: int
     bounding_box: BoundingBox
 
-    @staticmethod
-    def from_string(text: str) -> 'PageBoundingBoxList':
-        assert text
-        fragments = text.split(',')
-        assert len(fragments) == 5
-        return PageBoundingBox(
-            page_number=int(fragments[0]),
-            bounding_box=BoundingBox(
-                x=float(fragments[1]),
-                y=float(fragments[2]),
-                width=float(fragments[3]),
-                height=float(fragments[4])
-            )
-        )
-
 
 class PageBoundingBoxList(NamedTuple):
     page_bounding_box_list: Sequence[PageBoundingBox]
 
-    @staticmethod
-    def from_string(text: str) -> 'PageBoundingBoxList':
-        if not text:
-            return PageBoundingBoxList([])
-        fragments = text.split(';')
-        return PageBoundingBoxList([
-            PageBoundingBox.from_string(fragment)
-            for fragment in fragments
-        ])
-
     def __len__(self) -> int:
         return len(self.page_bounding_box_list)
+
+
+def parse_page_bounding_box(text: str) -> PageBoundingBox:
+    assert text
+    fragments = text.split(',')
+    assert len(fragments) == 5
+    return PageBoundingBox(
+        page_number=int(fragments[0]),
+        bounding_box=BoundingBox(
+            x=float(fragments[1]),
+            y=float(fragments[2]),
+            width=float(fragments[3]),
+            height=float(fragments[4])
+        )
+    )
+
+
+def parse_page_bounding_box_list(text: str) -> PageBoundingBoxList:
+    if not text:
+        return PageBoundingBoxList([])
+    fragments = text.split(';')
+    return PageBoundingBoxList([
+        parse_page_bounding_box(fragment)
+        for fragment in fragments
+    ])
--- tests/evaluation/scoring_methods/bounding_box_intersection_test.py
+++ tests/evaluation/scoring_methods/bounding_box_intersection_test.py
@@ -1,7 +1,7 @@
 from sciencebeam_judge.evaluation.scoring_methods.bounding_box_intersection import (
     BoundingBox,
     PageBoundingBox,
-    PageBoundingBoxList
+    parse_page_bounding_box_list
 )
 
 
@@ -40,19 +40,19 @@ class TestBoundingBox:
         )
 
 
-class TestPageBoundingBoxList:
+class TestParsePageBoundingBoxList:
     def test_should_parse_empty_none(self):
-        result = PageBoundingBoxList.from_string(None)
+        result = parse_page_bounding_box_list(None)
         assert not result.page_bounding_box_list
         assert not result
 
     def test_should_parse_empty_string(self):
-        result = PageBoundingBoxList.from_string('')
+        result = parse_page_bounding_box_list('')
         assert not result.page_bounding_box_list
         assert not result
 
     def test_should_parse_single_bounding_box(self):
-        result = PageBoundingBoxList.from_string(
+        result = parse_page_bounding_box_list(
             '101,102.22,103.33,104.44,105.55'
         )
         assert result.page_bounding_box_list
@@ -67,7 +67,7 @@ class TestPageBoundingBoxList:
         )
 
     def test_should_parse_multiple_bounding_box(self):
-        result = PageBoundingBoxList.from_string(
+        result = parse_page_bounding_box_list(
             '101,102.22,103.33,104.44,105.55;'
             '201,202.22,203.33,204.44,205.55'
         )
