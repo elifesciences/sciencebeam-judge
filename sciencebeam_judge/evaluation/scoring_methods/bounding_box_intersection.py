120974073d918fad834df455be1c9b90e789c33b
Daniel Ecer <de-code@users.noreply.github.com>
2021-09-20 16:10:41 +0100

    moved to separate bounding box module

--- sciencebeam_judge/evaluation/scoring_methods/bounding_box_intersection.py
+++ sciencebeam_judge/evaluation/scoring_methods/bounding_box_intersection.py
@@ -1,63 +1,8 @@
-from typing import NamedTuple, Sequence
-
-
-class BoundingRange(NamedTuple):
-    start: float
-    length: float
-
-    def validate(self) -> 'BoundingRange':
-        if self.length < 0:
-            raise ValueError(f'length must not be less than zero, was: {self.length}')
-        return self
-
-    def intersection(self, other: 'BoundingRange') -> 'BoundingRange':
-        intersection_start = max(self.start, other.start)
-        intersection_end = min(self.start + self.length, other.start + other.length)
-        return BoundingRange(
-            intersection_start,
-            max(0, intersection_end - intersection_start)
-        )
-
-
-class BoundingBox(NamedTuple):
-    x: float
-    y: float
-    width: float
-    height: float
-
-    @property
-    def area(self) -> float:
-        return self.width * self.height
-
-    @property
-    def x_range(self):
-        return BoundingRange(self.x, self.width).validate()
-
-    @property
-    def y_range(self):
-        return BoundingRange(self.y, self.height).validate()
-
-    def intersection(self, other: 'BoundingBox') -> 'BoundingBox':
-        intersection_x_range = self.x_range.intersection(other.x_range)
-        intersection_y_range = self.y_range.intersection(other.y_range)
-        return BoundingBox(
-            intersection_x_range.start,
-            intersection_y_range.start,
-            intersection_x_range.length,
-            intersection_y_range.length
-        )
-
-
-class PageBoundingBox(NamedTuple):
-    page_number: int
-    bounding_box: BoundingBox
-
-
-class PageBoundingBoxList(NamedTuple):
-    page_bounding_box_list: Sequence[PageBoundingBox]
-
-    def __len__(self) -> int:
-        return len(self.page_bounding_box_list)
+from sciencebeam_judge.utils.bounding_box import (
+    BoundingBox,
+    PageBoundingBox,
+    PageBoundingBoxList
+)
 
 
 def parse_page_bounding_box(text: str) -> PageBoundingBox:
--- tests/evaluation/scoring_methods/bounding_box_intersection_test.py
+++ tests/evaluation/scoring_methods/bounding_box_intersection_test.py
@@ -5,41 +5,6 @@ from sciencebeam_judge.evaluation.scoring_methods.bounding_box_intersection impo
 )
 
 
-class TestBoundingBox:
-    def test_should_calculate_area(self):
-        bounding_box = BoundingBox(
-            x=101, y=102, width=200, height=50
-        )
-        assert bounding_box.area == 200 * 50
-
-    def test_should_calculate_intersection_with_identical_bounding_box(self):
-        bounding_box = BoundingBox(110, 120, 50, 60)
-        assert (
-            bounding_box.intersection(bounding_box) == bounding_box
-        )
-
-    def test_should_calculate_intersection_with_smaller_contained_bounding_box(self):
-        assert (
-            BoundingBox(100, 100, 200, 200).intersection(
-                BoundingBox(110, 120, 50, 60)
-            ) == BoundingBox(110, 120, 50, 60)
-        )
-
-    def test_should_calculate_intersection_with_larger_bounding_box(self):
-        assert (
-            BoundingBox(110, 120, 50, 60).intersection(
-                BoundingBox(100, 100, 200, 200)
-            ) == BoundingBox(110, 120, 50, 60)
-        )
-
-    def test_should_calculate_intersection_with_overlapping_bounding_box(self):
-        assert (
-            BoundingBox(110, 120, 50, 60).intersection(
-                BoundingBox(120, 110, 100, 100)
-            ) == BoundingBox(120, 120, 40, 60)
-        )
-
-
 class TestParsePageBoundingBoxList:
     def test_should_parse_empty_none(self):
         result = parse_page_bounding_box_list(None)
